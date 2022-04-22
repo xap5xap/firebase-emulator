a80c0d9652ac6972a38ec9aaa5526ed944980d89
Xavier Perez <xap5xap@users.noreply.github.com>
2022-04-22 14:16:25 -0500

    wait for item to show

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,9 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import App from "./App";
 import userEvent from "@testing-library/user-event";
 
 jest.setTimeout(100000);
-
-export const sleep = (milliseconds) => {
-  return new Promise((resolve) => setTimeout(resolve, milliseconds));
-};
 
 export const wait = (milliseconds) => {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
@@ -16,7 +12,8 @@ export const wait = (milliseconds) => {
 describe("prueba", () => {
   it("should show new items", async () => {
     const user = userEvent.setup();
-    const { findAllByTestId, getByRole, queryByTestId } = render(<App />);
+    const { findAllByTestId, getByRole, getAllByTestId, queryByTestId } =
+      render(<App />);
 
     expect(queryByTestId("item")).toBeNull();
     await user.click(getByRole("button", { name: "Add item to list" }));
@@ -24,8 +21,12 @@ describe("prueba", () => {
 
     console.log("Adding one done");
     await user.click(getByRole("button", { name: "Add item to list" }));
-    await sleep(3000);
 
-    expect((await findAllByTestId("item")).length).toBe(2);
+    await waitFor(
+      () => {
+        expect(getAllByTestId("item").length).toBe(2);
+      },
+      { timeout: 3000 }
+    );
   });
 });
